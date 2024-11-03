d8c0bb6b1635bbd57366825a0fbce6a93de111cb
Gennadiy <renardes@gmail.com>
2024-11-03 13:05:48 +0500

    2024.10.27: правки оформления

--- 2024.10.27/1.py
+++ 2024.10.27/1.py
@@ -1,8 +1,11 @@
 name = input("Введите имя: ")
 surname = input("Введите фамилию: ")
 year_of_birth = int(input("Введите год рождения: "))
-print(surname, name + ',' , 2024 - year_of_birth )
+print(surname, name + ',' , 2024 - year_of_birth)
+
+
 # Введите имя: Иван
 # Введите фамилию: Петров
 # Введите год рождения: 2009
 # Петров Иван, 15
+
